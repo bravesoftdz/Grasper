d5cc21fdfb8d8e1b960281d40f960112edd7dd54
alezzle <niekras@tut.by>
2017-06-02 18:09:33 +0300

    dev

---
+++ DB/scripts.sql
@@ -0,0 +1,17 @@
+# отключить проверку внешних ключей
+SET foreign_key_checks = 0
+
+CREATE DEFINER=`root`@`localhost` TRIGGER `pia-dev`.`DeleteLinkRule`
+  AFTER DELETE ON `pia-dev`.`job_rule_links`
+  FOR EACH ROW
+BEGIN
+	delete from job_rules where id = old.job_rule_id;
+END;
+
+
+CREATE DEFINER=`root`@`localhost` TRIGGER `pia-dev`.`DeleteRecRule`
+  AFTER DELETE ON `pia-dev`.`job_rule_records`
+  FOR EACH ROW
+BEGIN
+	delete from job_rules where id = old.job_rule_id;
+END;
---
+++ Delphi/ParseItAll/ParseItAll.dpr
@@ -0,0 +1,38 @@
+program ParseItAll;
+
+
+
+uses
+  Vcl.Forms,
+  vMain in 'vMain.pas' {ViewMain},
+  cController in 'cController.pas',
+  vLogin in 'vLogin.pas' {ViewLogin},
+  mLogin in 'mLogin.pas',
+  eEntities in 'eEntities.pas',
+  vJob in 'vJob.pas' {ViewJob},
+  ceflib,
+  mJobs in 'mJobs.pas',
+  vRules in 'vRules.pas' {ViewRules},
+  mRules in 'mRules.pas',
+  API_MVC in '..\..\..\Libraries\Delphi\API_MVC.pas',
+  API_MVC_Bind in '..\..\..\Libraries\Delphi\API_MVC_Bind.pas',
+  API_ORM in '..\..\..\Libraries\Delphi\API_ORM.pas',
+  API_DB in '..\..\..\Libraries\Delphi\API_DB.pas',
+  API_MVC_DB in '..\..\..\Libraries\Delphi\API_MVC_DB.pas',
+  API_DB_MySQL in '..\..\..\Libraries\Delphi\API_DB_MySQL.pas',
+  API_Files in '..\..\..\Libraries\Delphi\API_Files.pas',
+  API_ORM_Cntrls in '..\..\..\Libraries\Delphi\API_ORM_Cntrls.pas';
+
+{$R *.res}
+
+begin
+  CefCache := 'cache';
+  CefSingleProcess := False;
+  if not CefLoadLibDefault then
+    Exit;
+
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TViewMain, ViewMain);
+  Application.Run;
+end.
