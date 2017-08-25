29d2556a4364f675efdca97b8b8d03bc302b3728
alezzle <niekras@tut.by>
2017-08-25 17:20:20 +0300

    dev

--- JS/DOMParser.js
+++ JS/DOMParser.js
@@ -1,3 +1,15 @@
 console.log('domparser');
 
 var level = %s;
+
+function parseDOMbyLevel(level) {
+
+    level.rules.forEach(function (rule) {
+        alert(rule.id);
+    });
+
+}
+
+app.parsedataback(parseDOMbyLevel(level));
+
+console.log('done');
