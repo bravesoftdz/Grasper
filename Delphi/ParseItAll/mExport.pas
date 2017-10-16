7b276f66d92638852434e1e00ecfd8e9de899182
alezzle <niekras@tut.by>
2017-10-16 08:21:59 +0300

    dev

--- Delphi/ParseItAll/mExport.pas
+++ Delphi/ParseItAll/mExport.pas
@@ -16,6 +16,14 @@ type
 
     FTranslater: TYaTranslater;
 
+    FRuTitle: string;
+    FEnTitle: string;
+    FUaTitle: string;
+
+    FRuCity: string;
+    FEnCity: string;
+    FUaCity: string;
+
     function GetRuleKeysFromLevel(aLevel: TJobLevel): TArray<string>;
     function GetKeysFromRule(aRule: TJobRule): TArray<string>;
     procedure GetLinkslist(aJobID: Integer; aKeys: TArray<string>);
@@ -45,17 +53,43 @@ var
   Lang: string;
   Rec: TRecord;
 begin
-  if aValueStrings <> '' then Exit;
+  //if aValueStrings <> '' then Exit;
 
-  if aKey = 'en_title' then ruKey := 'ru_title';
-  if aKey = 'en_content' then ruKey := 'ru_content';
+  if (aKey = 'ru_title') then
+    if (aValueStrings.Length < 5) then
+      ruKey := 'en_title'
+    else
+      exit;
+
   if aKey = 'en_country' then ruKey := 'ru_country';
-  if aKey = 'en_address' then ruKey := 'ru_address';
-
   if aKey = 'ua_title' then ruKey := 'ru_title';
-  if aKey = 'ua_content' then ruKey := 'ru_content';
   if aKey = 'ua_country' then ruKey := 'ru_country';
   if aKey = 'ua_address' then ruKey := 'ru_address';
+  if aKey = 'ua_city' then ruKey := 'ru_city';
+
+  if aKey = 'ru_content' then
+    aValueStrings := Format(aValueStrings, [
+      FRuTitle,
+      FRuCity,
+      FRuTitle,
+      FRuCity
+    ]);
+
+  if aKey = 'en_content' then
+    aValueStrings := Format(aValueStrings, [
+      FEnTitle,
+      FEnCity,
+      FEnTitle,
+      FEnCity
+    ]);
+
+  if aKey = 'ua_content' then
+    aValueStrings := Format(aValueStrings, [
+      FUaTitle,
+      FUaCity,
+      FUaTitle,
+      FUaCity
+    ]);
 
   if ruKey = '' then Exit;
 
@@ -66,6 +100,7 @@ begin
     end;
 
   if aKey.Contains('en_') then Lang := 'en';
+  if aKey.Contains('ru_') then Lang := 'ru';
   if aKey.Contains('ua_') then Lang := 'uk';
 
   if ruValue <> '' then
@@ -105,6 +140,7 @@ var
   CSVString, ValueStrings: string;
   RecList: TObjectList<TRecord>;
   Rec: TRecord;
+  i: Integer;
 begin
   Link := TLink.Create(FDBEngine, aLinkID);
   try
@@ -125,6 +161,44 @@ begin
         if Key = 'ua_source' then
           ValueStrings := Link.Link
         else
+        if Key = 'category_identifier' then
+          ValueStrings := 'Hotels'
+        else
+        if Key = 'ru_content' then
+          ValueStrings :=
+        '%s– отличная гостиница в городе %s для приема как жителей'+
+        ' так и гостей города. Просторное здание, уютные номера с прекрасным ремонтом и'+
+        ' современной техникой позволят замечательно отдохнуть, переночевать или просто'+
+        ' отвлечься. Огромный опыт работы и профессиональная команда сотрудников отеля'+
+        ' помогут в решении любых вопросов по заселению, размещению, оформлению'+
+        ' документов и Вы получите гарантированно положительный результат. '+
+        'Вы точно останетесь довольны качеством обслуживания нашего отеля и будете'+
+        ' рекомендовать %s из города %s своим друзьям и знакомым.'
+        else
+        if Key = 'en_content' then
+          ValueStrings :=
+        '%s – a perfect hotel in the city %s for accommodation'+
+        ' of both residents  and visitors of the city. Spacious edifice, comfortable'+
+        ' rooms with beautiful furnishing and modern appliances provide superb leisure,'+
+        ' overnight stay and recreation. Huge practical experience and professional'+
+        ' team of employees provide assistance in settling the matters of accommodation,'+
+        ' allocation and drawing up of documents, securing your guaranteed positive'+
+        ' result.  You will be definitely satisfied by the quality of service in our'+
+        ' hotel and will recommend %s from the city %s to your'+
+        ' friends and acquaintances.'
+        else
+        if Key = 'ua_content' then
+          ValueStrings :=
+        '%s – чудовий готель в місті %s для прийому як мешканців'+
+        ' так і гостей міста. Простора будівля, комфортні номера з чудовим ремонтом і'+
+        ' сучасною технікою, дадуть можливість чудесно відпочити, переночувати чи'+
+        ' просто відволіктись від буденності. Багатий стаж роботи і професійна команда'+
+        ' персоналу готелю, допоможуть у вирішенні різних питань відносно заселення, '+
+        'розміщення, оформлення документів і Ви обов’язково отримаєте позитивний'+
+        ' результат. Ви залишитеся задоволеними якістю обслуговування нашого готелю'+
+        ' і будете  рекомендувати %s з міста %s своїм друзям і'+
+        ' знайомим.'
+        else
           begin
             RecList := Link.GetRecordsByKey(Key);
             try
@@ -140,7 +214,24 @@ begin
           end;
 
         // translate
-        //TryGetTranslate(ValueStrings, Key, Link);
+        TryGetTranslate(ValueStrings, Key, Link);
+        if Key = 'ru_title' then
+            FRuTitle := ValueStrings;
+
+        if Key = 'en_title' then
+            FEnTitle := ValueStrings;
+
+        if Key = 'ua_title' then
+            FUaTitle :=  ValueStrings;
+
+        if Key = 'ru_city' then
+            FRuCity := ValueStrings;
+
+        if Key = 'en_city' then
+            FEnCity := ValueStrings;
+
+        if Key = 'ua_city' then
+            FUaCity :=  ValueStrings;
 
         AddToCSVString(CSVString, ValueStrings);
       end;
@@ -218,6 +309,10 @@ begin
       Level := Job.Levels[i];
       Keys := Keys + GetRuleKeysFromLevel(Level);
     end;
+  Keys := Keys + ['category_identifier', 'ua_title', 'ua_city', 'ua_country', 'ua_address'];
+  Keys := Keys + ['en_country'];
+  Keys := Keys + ['ru_content', 'en_content', 'ua_content'];
+  Keys := Keys + ['ru_contacts', 'en_contacts', 'ua_contacts'];
   Keys := Keys + ['ru_source', 'en_source', 'ua_source'];
 
   // create file
